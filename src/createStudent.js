e4cb7ff86252d31827a33ae1321a8060975ef827
RamosFonseca <beidseitig@gmail.com>
2022-01-18 10:44:51 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => ({
+  name: studentName,
+  feedback: () => 'Eita pessoa boa!',
+});
+
+console.log(createStudent('Ramos'));
 
 module.exports = createStudent;
