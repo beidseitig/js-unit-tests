ebc8dbec9bddf0f7cef9bccd9034356683a19dc0
RamosFonseca <beidseitig@gmail.com>
2022-01-18 09:24:05 -0300

    Ajustando requisito 1

--- src/average.js
+++ src/average.js
@@ -17,13 +17,13 @@ const average = (array) => {
   if (array.length === 0) {
     return undefined;
   }
-  for (let index in array) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
       return undefined;
     }
     sum += array[index];
   }
-  let averageNum = Math.round(sum / array.length)
+  let averageNum = Math.round(sum / array.length);
   return averageNum;
 };
 
